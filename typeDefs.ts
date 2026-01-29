55cbc84fb9210f21b149a4ff918f8c1635dc7c89
vanphuTteam <vanphu120107@gmail.com>
2026-01-29 07:33:55 +0700

    thêm, sửa bài viết

--- typeDefs.ts
+++ typeDefs.ts
@@ -26,7 +26,8 @@ export const typeDefs = `#graphql
     input ArticleInput {
         title: String,
         avatar: String,
-        description: String
+        description: String,
+        categoryId: String
     }
 
     input CategoryInput {
